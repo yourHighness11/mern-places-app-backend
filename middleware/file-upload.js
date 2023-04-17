48e10b2bea229fef8ea3e7bdfdfe2d84d7ee80b8
Sarvesh <coolsarveshkirodiwal111@gmail.com>
2023-04-17 15:11:25 +0530

    sdads

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -1,5 +1,5 @@
 const multer = require("multer");
-// const { v1: uuid } = require("uuid");
+const { v1: uuid } = require("uuid");
 const MIME_TYPE_MAP = {
   "image/png": "png",
   "image/jpg": "jpg",
@@ -14,7 +14,7 @@ const fileUpload = multer({
     },
     filename: (req, file, cb) => {
       const ext = MIME_TYPE_MAP[file.mimetype];
-      cb(null, file.filename);
+      cb(null, uuid() + "." + ext);
     },
   }),
   fileFilter: (req, file, cb) => {
